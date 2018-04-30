646b1b6350c2b02c61f390c66621cce1b411a739
Mostafa Rizk <m.rizk.student@gmail.com>
2018-04-30 08:43:59 -0600

    Added mcnugget numbers (6,9,20) to arrays

--- mcdiophantine.java
+++ mcdiophantine.java
@@ -3,7 +3,11 @@ import java.util.*;
 public class mcdiophantine {
    public static void main(String args[]) {
       int[][] parts = new int[100][3];
+      parts[0][0] = 1; parts[0][1] = 0; parts[0][2] = 0;
+      parts[1][0] = 0; parts[1][1] = 1; parts[1][2] = 0;
+      parts[2][0] = 0; parts[2][1] = 0; parts[2][2] = 1;
       int[] nuggets = new int[100];
+      nuggets[0] = 6; nuggets[1] = 9; nuggets[2] = 20;
       int pos1, pos2, quotient, sum, marker = 3, n = 6;
       boolean inList;
    }
