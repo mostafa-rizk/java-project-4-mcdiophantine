1ec26c3ce375c982ef029eeb6726618e8ace672d
Mostafa Rizk <m.rizk.student@gmail.com>
2018-04-30 08:41:26 -0600

    Created necessary variables

--- mcdiophantine.java
+++ mcdiophantine.java
@@ -2,6 +2,9 @@ import java.io.*;
 import java.util.*;
 public class mcdiophantine {
    public static void main(String args[]) {
-      
+      int[][] parts = new int[100][3];
+      int[] nuggets = new int[100];
+      int pos1, pos2, quotient, sum, marker = 3, n = 6;
+      boolean inList;
    }
 }
